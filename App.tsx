06bd1269379f5aababe5ae365b566e7291f5cfc9
Felipe Kruschewsky <9900928@prestadorserv.com.br>
2023-12-28 17:52:46 -0300

    Criação e estilização da tela  inicial com componentes

--- App.tsx
+++ App.tsx
@@ -1,7 +1,24 @@
-import { Groups } from '@screens/Groups';
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from '@components/Loading';
+
+import theme from './src/theme';
+
+import { NewGroup } from '@screens/NewGroup';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      { fontsLoaded ? <NewGroup /> : <Loading />}
+    </ThemeProvider>
   );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
+
+import { Container } from './styles';
 
 export function Groups() {
+    const [groups, setGroups] = useState<string[]>([]);
+
     return (
-        <View style={styles.container}>
-        <Text>Groups</Text>
-        </View>
+        <Container>
+            <Header />
+
+            <Highlight 
+                title='Turmas'
+                subtitle='Jogue com a sua turma'
+            />
+
+            <FlatList 
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({item}) => (
+                    <GroupCard 
+                        title={item} 
+                    />
+                )}
+                contentContainerStyle={groups.length === 0 && { flex: 1 }}
+                ListEmptyComponent={() => (
+                    <ListEmpty 
+                        message="Que tal cadastrar a primeira turma?"
+                    />
+                )}
+            />
+            <Button 
+                title="Criar nova turma"
+            />
+        </Container>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
